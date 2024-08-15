53833a14ce529cb53f6f99df44629bd63692aaf1
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-15 15:21:02 +0200

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Have luck in the bootcamp!");
+
+        String textOfHippHurra = "Hipp, Hipp, Hurra!!";
+        // 150 Zeilen Code
+
+        System.out.println(textOfHippHurra);
+        System.out.println(textOfHippHurra);
+        System.out.println(textOfHippHurra);
     }
 }
