7c6eaf239e18d1f26592b1c97079daf48d704089
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-15 15:50:37 +0200

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -8,8 +10,47 @@ public class Main {
         String textOfHippHurra = "Hipp, Hipp, Hurra!!";
         // 150 Zeilen Code
 
+        System.out.println(textOfHippHurra.toUpperCase(Locale.ROOT));
         System.out.println(textOfHippHurra);
         System.out.println(textOfHippHurra);
-        System.out.println(textOfHippHurra);
+
+        // primitive Datentypen
+
+        // integer (Zahlen)
+
+        int number = 10;
+        int number2 = 12;
+        System.out.println(number+number2);
+
+        // integer mit mehreren Stellen
+
+        double numberWithDecimal = 10.123445;
+        System.out.println(numberWithDecimal);
+
+        boolean trafficLightIsGreen = false;
+        if (trafficLightIsGreen) {
+            System.out.println("Traffic light is green");
+        } else {
+            System.out.println("Traffic light is red");
+        }
+
+        byte myNumber = 127;
+        System.out.println(myNumber);
+        short myBiggerNumber = -32768;
+        System.out.println(myBiggerNumber);
+
+        // logical operators
+
+        boolean itIsRainingToday = true;
+        boolean itIsRainingTomorrow = false;
+
+        // && operator (true if both vaules are true)
+        System.out.println("zwei Tage regen? " + (itIsRainingToday && itIsRainingTomorrow));
+
+        // || operator ( returns true, if one boolean is true
+        System.out.println("mindestens ein tag regen? " + (itIsRainingToday || itIsRainingTomorrow));
+
+        // ! "switch" booleans
+        System.out.println("Radtour heute möglich?" + (!itIsRainingToday));
     }
 }
